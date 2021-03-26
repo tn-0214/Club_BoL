b64a7e40cc592c8742ec136b57b317162931c983
tn-0214 <wataru.nishikiori1209@gmail.com>
2021-03-27 01:41:35 +0900

    旧コード引き継ぎ〜郵便番号自動入力の実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,18 @@
 class ApplicationController < ActionController::Base
+  before_action :configre_permitted_parameters, if: :devise_controller?
+
+  def configre_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [
+      :first_name,
+      :last_name,
+      :nickname,
+      :birthday,
+      :phone_number,
+      :postcode,
+      :prefecture_name,
+      :address_city,
+      :address_street,
+      :address_building
+      ])
+  end
 end
