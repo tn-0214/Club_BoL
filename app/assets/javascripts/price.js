2c60b56214dea5f76c1118421fb1f9480a628f28
tn-0214 <79251508+tn-0214@users.noreply.github.com>
2021-04-15 13:04:57 +0900

    jsファイルの微修正

--- app/assets/javascripts/price.js
+++ app/assets/javascripts/price.js
@@ -1,15 +1,18 @@
-window.addEventListener('load', () => {
-  const priceInput = document.getElementById("lending-period");
-  priceInput.addEventListener("input", () => {
+
+window.addEventListener("DOMContentLoaded", () => {
+  const priceInput = document.getElementById('lending-period');
+  if(priceInput != null){
+  priceInput.addEventListener('input', () => {
     const inputValue = priceInput.value;
 
-    const addTaxDom = document.getElementById("item-daily-price");
+    const addTaxDom = document.getElementById('item-daily-price');
       const DailyPrice = gon.daily_price
     addTaxDom.innerHTML = DailyPrice ;
 
-    const profitAmount = document.getElementById("amount");
+    const profitAmount = document.getElementById('amount');
     profitAmount.innerHTML = Math.floor(inputValue * DailyPrice); 
 
     
 })
-});
+}})
+;
